3bfb033f423ba91b71cc5d53f14c3d66d538dcb9
Dan Barber <github@danbarber.me>
2015-06-17 18:01:54 +0100

    List drafts.

--- lib/jekyll_web/webserver.rb
+++ lib/jekyll_web/webserver.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
 require 'sinatra/asset_pipeline'
+require 'sinatra/config_file'
 require 'sass'
 
 module JekyllWeb
   class Webserver < Sinatra::Base
+    register Sinatra::ConfigFile
+    register Sinatra::AssetPipeline
+    register Sinatra::Namespace
+
+    config_file '../../config.yml'
 
     set server: 'puma'
 
@@ -12,9 +18,6 @@ module JekyllWeb
     set :assets_precompile, %w(app.js app.css *.png *.jpg *.svg *.eot *.ttf *.woff)
     set :assets_prefix, ['assets']
 
-    register Sinatra::AssetPipeline
-    register Sinatra::Namespace
-
     get '/' do
       "Hello, World!"
     end
@@ -22,6 +25,13 @@ module JekyllWeb
     get '/env' do
       content_type :json
       ENV.to_hash.to_json
+    end
+
+    get '/drafts' do
+      dir = Dir.new("#{settings.site_path}/#{settings.drafts_dir}")
+      drafts = dir.each.select { |e| e[/#{settings.post_ext}$/] }
+      content_type :json
+      drafts.to_json
     end
   end
 end
