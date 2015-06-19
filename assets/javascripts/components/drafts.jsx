f2ae55514bfc66e31aa72baca307f3cb799a607c
Dan Barber <github@danbarber.me>
2015-06-19 17:54:45 +0100

    Unsubscribe components on unmount.

--- assets/javascripts/components/drafts.jsx
+++ assets/javascripts/components/drafts.jsx
@@ -1,13 +1,22 @@
 var Drafts = React.createClass({
+
   getInitialState: function () {
     return { items: [] }
   },
+
   componentDidMount: function () {
+    // returns an unsubscribe handler
     this.unsubscribe = draftsStore.listen(this.updateItems);
   },
+
+  componentWillUnmount: function() {
+    this.unsubscribe();
+  },
+
   updateItems: function (items) {
     this.setState({ items: items });
   },
+
   render: function () {
     var drafts = this.state.items.map(function (draft) {
       return (
--- assets/javascripts/components/posts.jsx
+++ assets/javascripts/components/posts.jsx
@@ -1,13 +1,22 @@
 var Posts = React.createClass({
+
   getInitialState: function () {
     return { items: [] }
   },
+
   componentDidMount: function () {
+    // returns an unsubscribe handler
     this.unsubscribe = postsStore.listen(this.updateItems);
   },
+
+  componentWillUnmount: function() {
+    this.unsubscribe();
+  },
+
   updateItems: function (items) {
     this.setState({ items: items });
   },
+
   render: function () {
     var posts = this.state.items.map(function (post) {
       return (
